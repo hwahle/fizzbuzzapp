a7dcdbbe1554b2f6f970e1aed95e994f9fceab5b
hwahle <helenwahle@gmail.com>
2015-10-05 10:43:03 -0700

    initial js commit

---
+++ js/app.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+	console.log('Hello');
+
+for(var i = 1; i <=100; ++i){
+
+ if(i % 15 === 0){
+
+        
+        document.write('Fizz Buzz');
+        document.write('<br/>');
+
+    }else if(i % 3 === 0){
+
+        
+        document.write('Fizz');
+        document.write('<br/>');
+
+    }else if(i % 5 === 0){
+
+        
+        document.write('Buzz');
+        document.write('<br/>');
+
+    }else{
+
+      
+        document.write(i);
+        document.write('<br/>');
+    }
+}
+
+
+});
