9ce59b0b39750c4ff6f67302af39f86c5ce72b9f
hwahle <helenwahle@gmail.com>
2015-10-07 23:29:34 -0700

    refracted

--- js/app.js
+++ js/app.js
@@ -1,33 +1,36 @@
 $(document).ready(function() {
-	console.log('Hello');
+	var userInput = window.prompt("Enter a number");
+	var userNumber = parseInt(userInput);
+	
+	
+		for(var i = 1; i <= userNumber; i++) {
+			
 
-for(var i = 1; i <=100; ++i){
-
- if(i % 15 === 0){
-
-        
-        $('#fizzbuzz').append('Fizz Buzz' + '<br />');
-        
-
-    }else if(i % 3 === 0){
-
-        
-        $('#fizzbuzz').append('Fizz' + '<br />');
-        
-
-    }else if(i % 5 === 0){
-
-        
-        $('#fizzbuzz').append('Buzz' + '<br />');
-        
-
-    }else{
-
-      
-        $('#fizzbuzz').append(i + '<br />');
-        
-    }
-}
+			if(i % 15 === 0){
 
 
-});
+				$('#fizzbuzz').append('Fizz Buzz' + '<br />');
+
+
+			}else if(i % 3 === 0){
+
+
+				$('#fizzbuzz').append('Fizz' + '<br />');
+
+
+			}else if(i % 5 === 0){
+
+
+				$('#fizzbuzz').append('Buzz' + '<br />');
+
+
+			}else{
+
+
+				$('#fizzbuzz').append(i + '<br />');
+
+			}
+		}
+	})
+
+
